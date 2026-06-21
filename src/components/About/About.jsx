ed0aacd89babeb97a9f4d203e920feb355662e5b
NamanBabbar2701 <MITU22BTCS0467@students.mituniversity.edu.in>
2026-06-22 00:08:05 +0530

    About Section with Profile Image Tilt

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,22 +1,87 @@
-import React from 'react'
+import React from "react";
+import { TypeAnimation } from "react-type-animation";
+import Tilt from 'react-parallax-tilt'
+import profileImage from '../../assets/namanprofile.png'
 
 const About = () => {
-    return(
-       <section 
-       id='about'
-       className='py-4 px-[7vw] md:px=[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'
-       >
-        <div className='flex flex-col-reverse md:flex-row justify-between item-center'>
-            {/* Left Side */}
-            <div className='md:w-1/2 text-center md:text-left mt-8 md:mt-0'>
-            {/* Greetings */}
+  return (
+    <section
+      id="about"
+      className="py-4 px-[7vw] md:px-[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32"
+    >
+      <div className="flex flex-col-reverse md:flex-row justify-between items-center">
+        {/* Left Side */}
+        <div className="md:w-1/2 text-center md:text-left mt-8 md:mt-0">
+          {/* Greeting */}
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold text-white mb-2 leading-tight">
+            Hi, I am
+          </h1>
+          {/* Name */}
+          <h2 className="text-4xl sm:text-5xl md:text-6xl font-bold text-white mb-4 leading-tight ">
+            Naman Babbar
+          </h2>
+          {/* Skills heading with typing effect */}
+          <h3 className="text-xl sm:text-2xl md:text-3xl font-semibold mb-4 text-[#8245ec] leading-tight">
+            <span className="text-white">I am a&nbsp;</span>
+            <TypeAnimation
+              sequence={[
+                500, //typingDelay
+                "Coder",
+                2000, //eraseDelay
+                "Backend Developer",
+                2000,
+                "DSA Aspirant",
+                2000,
+                "ML Engineer",
+                2000,
+              ]}
+              speed={50}
+              //   deletionSpeed={50}
+              repeat={Infinity}
+              cursor={true}
+              className="text-[#8245ec] inline-block"
+            />
+          </h3>
+          {/* About me paragraph */}
+          <p className="text-base sm:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
+            Aspiring Software Engineer with a strong foundation in Java, Spring
+            Boot, React, and Data Structures & Algorithms. Passionate about
+            building efficient, user-centric applications and solving real-world
+            problems through technology.
+          </p>
+          {/* Resume Button */}
+          <a 
+          href="https://drive.google.com/file/d/1O5mrcuGTmUsY3Im8-jbaD258s-yA-RbN/view?usp=sharing"
+          target='_blank'
+          rel='noopener noreferrer'
+          className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+          style={{
+            background: 'linear-gradient(90deg, #8425ec, #a855f7)',
+            boxShadow: '0 0 2px #8425ec, 0 0 2px #8425ec, 0 0 40px #8425ec',
+          }}
+          >
+            DOWNLOAD CV
+          </a>
+        </div>
+        {/* Right Side */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+        <Tilt
+        className="w-48 h-48 sm:w-64 sm:h-64 md:w-[30rem] md:h-[30rem] border-4 border-purple-700 rounded-full"
+        tiltMaxAngleX={20}
+        tiltMaxAngleY={20}
+        perspective={1000}
+        scale={1.05}
+        transitionSpeed={1000}
+        gyroscope={true}
+        >
+        <img src={profileImage} alt="Naman Babbar"
+        className="w-full h-full rounded-full object-cover drop-shadow-[0_10px_20px_rgba[130,69,236,0.5]]"
+        />
+        </Tilt>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-            </div>
-            
-        </div> 
-
-       </section>
-    )
-}
-
-export default About
+export default About;
