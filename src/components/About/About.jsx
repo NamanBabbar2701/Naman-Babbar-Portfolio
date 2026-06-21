2872f13bbd047093044a390ec57ff68b2fef4e98
NamanBabbar2701 <MITU22BTCS0467@students.mituniversity.edu.in>
2026-06-21 22:44:40 +0530

    Animation Added

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const About = () => {
     return(
-        <div>
+       <section 
+       id='about'
+       className='py-4 px-[7vw] md:px=[7vw] lg:px-[20vw] font-sans mt-16 md:mt-24 lg:mt-32'
+       >
+        <div className='flex flex-col-reverse md:flex-row justify-between item-center'>
+            {/* Left Side */}
+            <div className='md:w-1/2 text-center md:text-left mt-8 md:mt-0'>
+            {/* Greetings */}
 
-        </div>
+            </div>
+            
+        </div> 
+
+       </section>
     )
 }
 
