5e93625d9d012713a96d33312413bd581b818e1b
NamanBabbar2701 <MITU22BTCS0467@students.mituniversity.edu.in>
2026-06-21 18:43:47 +0530

    First Commit: Navbar Done

---
+++ src/constants.js
@@ -0,0 +1,76 @@
+import htmllogo from './assets/tech_logo/html.png'
+import csslogo from './assets/tech_logo/css.png'
+import javascriptlogo from './assets/tech_logo/javascript.png'
+import reactjs from './assets/tech_logo/reactjs.png'
+import tailwindcsslogo from './assets/tech_logo/tailwindcss.png'
+import bootstraplogo from './assets/tech_logo/bootstrap.png'
+import springbootlogo from './assets/tech_logo/springboot.png'
+import nodejslogo from './assets/tech_logo/nodejs.png'
+import mysqllogo from './assets/tech_logo/mysql.png'
+import mongodblogo from './assets/tech_logo/mongodb.png'
+import clogo from './assets/tech_logo/C.png'
+import cpplogo from './assets/tech_logo/cpp.png'
+import javalogo from './assets/tech_logo/java.png'
+import pythonlogo from './assets/tech_logo/python.png'
+import gitlogo from './assets/tech_logo/git.png'
+import githublogo from './assets/tech_logo/github.png'
+import vscodelogo from './assets/tech_logo/vscode.png'
+import postmanlogo from './assets/tech_logo/postman.png'
+import netlifylogo from './assets/tech_logo/netlify.png'
+import vercellogo from './assets/tech_logo/vercel.png'
+import renderlogo from './assets/tech_logo/render.png'
+import compasslogo from './assets/tech_logo/mc.png'
+import atlaslogo from './assets/tech_logo/mdbatlas.png'
+import canvalogo from './assets/tech_logo/canva.png'
+import figmalogo from './assets/tech_logo/figma.png'
+import adobeexpresslogo from './assets/tech_logo/adobeexpress.png'
+
+export const SkillsInfo = [
+    {
+        title: 'Frontend',
+        skills: [
+            {name: 'HTML', logo: 'htmllogo'},
+            {name: 'CSS', logo: 'csslogo'},
+            {name: 'Javascript', logo: 'javascript'},
+            {name: 'React JS', logo: 'reactjslogo'},
+            {name: 'Tailwind CSS', logo: 'tailwindcsslogo'},
+            {name: 'Bootstrap', logo: 'bootstraplogo'},
+        ],
+    },
+    {
+        title: 'Backend',
+        skills: [
+            {name: 'Springboot', logo: 'springbootlogo'},
+            {name: 'Node JS', logo: 'nodejslogo'},
+            {name: 'MySQL', logo: 'mysqllogo'},
+            {name: 'MongoDB', logo: 'mongodblogo'},
+    ],
+    },
+    {
+        title: 'Languages',
+        skills: [
+            {name: 'C', logo: 'clogo'},
+            {name: 'C++', logo: 'cpplogo'},
+            {name: 'Java', logo: 'javalogo'},
+            {name: 'Pyhton', logo: 'pythonlogo'},
+            {name: 'Javascript', logo: 'javascriptlogo'},
+        ],
+    },
+    {
+        title: "Tools",
+        skills: [
+            {name: 'Git', logo: 'gitlogo'},
+            {name: 'GitHub', logo: 'githublogo'},
+            {name: 'VS Code', logo: 'vscodelogo'},
+            {name: 'Postman', logo: 'postmanlogo'},
+            {name: 'Compass', logo: 'compasslogo'},
+            {name: 'Atlas', logo: 'atlaslogo'},
+            {name: 'Vercel', logo: 'vercellogo'},
+            {name: 'Netlify', logo: 'netlifylogo'},
+            {name: 'Render', logo: 'renderlogo'},
+            {name: 'Figma', logo: 'figmalogo'},
+            {name: 'Canva', logo: 'canvalogo'},
+            {name: 'Adobe Express', logo: 'adobeexpresslogo'},
+        ],
+    },
+];
